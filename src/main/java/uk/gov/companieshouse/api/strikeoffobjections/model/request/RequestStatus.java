3947f249ed2673bc69f019d28d8ebad09b131175
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-06-29 13:12:53 +0100

    BI-4117 Model for strike off objections mongo db

---
+++ src/main/java/uk/gov/companieshouse/api/strikeoffobjections/model/request/RequestStatus.java
@@ -0,0 +1,10 @@
+package uk.gov.companieshouse.api.strikeoffobjections.model.request;
+
+public enum RequestStatus {
+    OPEN,
+    CHIPS_INCOMING_CONTACT_SENT,
+    EMAIL_SENT,
+    CHIPS_OUTGOING_EMAIL_CONTACT_SENT,
+    PROCESSED,
+    INELIGIBLE
+}
