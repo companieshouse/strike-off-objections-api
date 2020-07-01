5fb0ab3cecb2d853ad390d884016fbe80536f923
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-07-01 15:14:32 +0100

    Config to remove class from db data

---
+++ src/main/java/uk/gov/companieshouse/api/strikeoffobjections/config/SpringMongoConfig.java
@@ -0,0 +1,29 @@
+package uk.gov.companieshouse.api.strikeoffobjections.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class SpringMongoConfig {
+
+    /**
+     * This removes the _class field on entities saved to mongodb
+     */
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(@Autowired MongoMappingContext mongoMappingContext,
+                                                       @Autowired MongoDbFactory mongoDbFactory) {
+
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return converter;
+    }
+}
