cab6c7b11a2f009f239093a26888feda2d102f67
mwestacott <mwestacott1@companieshouse.gov.uk>
2020-07-10 14:10:11 +0100

    OBJ-65 Ensure correct JSON response content is returned (to match spec)

--- src/main/java/uk/gov/companieshouse/api/strikeoffobjections/model/response/AttachmentResponseDTO.java
+++ src/main/java/uk/gov/companieshouse/api/strikeoffobjections/model/response/AttachmentResponseDTO.java
@@ -1,18 +1,35 @@
 package uk.gov.companieshouse.api.strikeoffobjections.model.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+
 import uk.gov.companieshouse.service.links.Links;
 
 public class AttachmentResponseDTO {
 
+    @JsonProperty("id")
     private String id;
-    
-    private Links links;
-    
+
+    @JsonProperty("name")
     private String name;
-    
+
+    @JsonProperty("content_type")
     private String contentType;
     
+    @JsonProperty("size")
     private long size;
+
+    @JsonProperty("links")
+    @JsonUnwrapped
+    private Links links;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
