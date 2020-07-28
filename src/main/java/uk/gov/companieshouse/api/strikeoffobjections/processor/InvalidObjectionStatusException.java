e100c0501ecc539143c9b6d775e329bb6892c700
markpit <mpitten@companieshouse.gov.uk>
2020-07-28 15:17:19 +0100

    OBJ-159 Adding comments to InvalidObjectionStatusException

--- src/main/java/uk/gov/companieshouse/api/strikeoffobjections/processor/InvalidObjectionStatusException.java
+++ src/main/java/uk/gov/companieshouse/api/strikeoffobjections/processor/InvalidObjectionStatusException.java
@@ -1,5 +1,8 @@
 package uk.gov.companieshouse.api.strikeoffobjections.processor;
 
+/**
+ * Thrown when the Objection status is in an invalid state
+ */
 public class InvalidObjectionStatusException extends Exception {
 
      public InvalidObjectionStatusException(String message) {
