dfaadfa131533d427baf6c9496c85c028981b7ac
ICoxCH <ICox@companieshouse.gov.uk>
2020-08-25 15:49:25 +0100

    Hide default constructor

--- src/main/java/uk/gov/companieshouse/api/strikeoffobjections/interceptor/InterceptorConstants.java
+++ src/main/java/uk/gov/companieshouse/api/strikeoffobjections/interceptor/InterceptorConstants.java
@@ -1,6 +1,10 @@
 package uk.gov.companieshouse.api.strikeoffobjections.interceptor;
 
 public class InterceptorConstants {
+
+    private InterceptorConstants() {
+    }
+
     public static final String OBJECTION_ATTRIBUTE = "objection";
     public static final String COMPANY_NUMBER_PATH_VARIABLE = "companyNumber";
     public static final String OBJECTION_ID_PATH_VARIABLE = "objectionId";
