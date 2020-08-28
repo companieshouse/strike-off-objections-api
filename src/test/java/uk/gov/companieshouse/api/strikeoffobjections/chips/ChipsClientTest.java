d9f0a090a4f53598f85fd816661fab15d96cc812
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-08-28 13:24:04 +0100

    OBJ-239 adding request to the client stub

--- src/test/java/uk/gov/companieshouse/api/strikeoffobjections/chips/ChipsClientTest.java
+++ src/test/java/uk/gov/companieshouse/api/strikeoffobjections/chips/ChipsClientTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.api.strikeoffobjections.common.ApiLogger;
 import uk.gov.companieshouse.api.strikeoffobjections.groups.Unit;
+import uk.gov.companieshouse.api.strikeoffobjections.model.chips.ChipsRequest;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
@@ -17,6 +18,7 @@ import static org.mockito.Mockito.verify;
 class ChipsClientTest {
 
     private static final String REQUEST_ID = "test123";
+    private static final String COMPANY_NUMBER = "12345678";
 
     @InjectMocks
     private ChipsClient chipsClient;
@@ -26,9 +28,12 @@ class ChipsClientTest {
 
     @Test
     void testSendMessageToChips(){
-        // TODO OBJ-240 test sending contact data to CHIPS
-        chipsClient.sendToChips(REQUEST_ID);
+        // TODO OBJ-240 test sending contact data to CHIPS and capture request content
+        ChipsRequest chipsRequest = new ChipsRequest(
+                REQUEST_ID,
+                COMPANY_NUMBER
+        );
+        chipsClient.sendToChips(REQUEST_ID, chipsRequest);
         verify(apiLogger, times(1)).infoContext(anyString(), anyString());
     }
-
 }
