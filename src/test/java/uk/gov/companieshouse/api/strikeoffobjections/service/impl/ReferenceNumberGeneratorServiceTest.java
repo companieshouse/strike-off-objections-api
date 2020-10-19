b37981d8061a087fe5031feabd5afdd6391f5202
ICoxCH <ICox@companieshouse.gov.uk>
2020-10-19 12:45:41 +0100

    Only use UUID for increased randomness

--- src/test/java/uk/gov/companieshouse/api/strikeoffobjections/service/impl/ReferenceNumberGeneratorServiceTest.java
+++ src/test/java/uk/gov/companieshouse/api/strikeoffobjections/service/impl/ReferenceNumberGeneratorServiceTest.java
@@ -3,20 +3,15 @@ package uk.gov.companieshouse.api.strikeoffobjections.service.impl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.api.strikeoffobjections.groups.Unit;
 
-import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
 @Unit
 @ExtendWith(MockitoExtension.class)
@@ -25,17 +20,11 @@ class ReferenceNumberGeneratorServiceTest {
     private static final Set<Character> ALLOWED_CHARS = new HashSet<>
             (Arrays.asList('A','B','C','D','E','F','1','2','3','4','5','6','7','8','9','0', '-'));
 
-    private static final LocalDateTime LOCAL_DATE_TIME =
-            LocalDateTime.of(2020, Month.OCTOBER, 6, 9, 30);
-    @Mock
-    private Supplier<LocalDateTime> supplier;
-
     @InjectMocks
     private ReferenceNumberGeneratorService referenceNumberGeneratorService;
 
     @Test
     void generateIdTestLengthAndPrefix() {
-        when(supplier.get()).thenReturn(LOCAL_DATE_TIME);
         String referenceNumber = referenceNumberGeneratorService.generateReferenceNumber();
 
         assertEquals("OBJ-", referenceNumber.substring(0, 4));
@@ -44,7 +33,6 @@ class ReferenceNumberGeneratorServiceTest {
 
     @Test
     void generateIdTestOnlyAllowedCharsPresent() {
-        when(supplier.get()).thenReturn(LOCAL_DATE_TIME);
         String referenceNumber = referenceNumberGeneratorService.generateReferenceNumber();
         String referenceNumberNoPrefix = referenceNumber.substring(4);
 
@@ -55,7 +43,6 @@ class ReferenceNumberGeneratorServiceTest {
 
     @Test
     void generateIdTestHyphensInCorrectPlace() {
-        when(supplier.get()).thenReturn(LOCAL_DATE_TIME);
         String referenceNumber = referenceNumberGeneratorService.generateReferenceNumber();
         String referenceNumberNoPrefix = referenceNumber.substring(4);
 
