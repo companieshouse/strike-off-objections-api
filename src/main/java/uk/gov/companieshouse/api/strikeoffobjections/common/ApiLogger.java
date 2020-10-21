b3f52e473f4ada3bb589be83d1ff09a4a8e868e1
mwestacott <mwestacott1@companieshouse.gov.uk>
2020-10-21 15:05:09 +0100

    OBJ-210 Clone map passed to structured logging to stop duplicate message

--- src/main/java/uk/gov/companieshouse/api/strikeoffobjections/common/ApiLogger.java
+++ src/main/java/uk/gov/companieshouse/api/strikeoffobjections/common/ApiLogger.java
@@ -5,10 +5,13 @@ import uk.gov.companieshouse.api.strikeoffobjections.Application;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Acts as a wrapper for the structured logger to help with unit testing
+ * Acts as a wrapper for the structured logger to help with unit testing and also ensures that the
+ * map data structure passed to the Companies House logger is not changed if used by subsequent
+ * logging calls.
  */
 @Component
 public class ApiLogger {
@@ -20,7 +23,7 @@ public class ApiLogger {
     }
 
     public void debugContext(String context, String message, Map<String, Object> dataMap) {
-        LOGGER.debugContext(context, message, dataMap);
+        LOGGER.debugContext(context, message, cloneMapData(dataMap));
     }
 
     public void info(String message) {
@@ -32,7 +35,7 @@ public class ApiLogger {
     }
 
     public void infoContext(String context, String message, Map<String, Object> dataMap) {
-        LOGGER.infoContext(context, message, dataMap);
+        LOGGER.infoContext(context, message, cloneMapData(dataMap));
     }
 
     public void errorContext(String context, Exception e) {
@@ -44,6 +47,22 @@ public class ApiLogger {
     }
 
     public void errorContext(String context, String message, Exception e, Map<String, Object> dataMap) {
-        LOGGER.errorContext(context, message, e, dataMap);
+        LOGGER.errorContext(context, message, e, cloneMapData(dataMap));
+    }
+
+    /**
+     * The Companies House logging implementation modifies the data map content which means that
+     * if the same data map is used for subsequent calls any new message that might be passed in
+     * is not displayed in certain log format outputs. Creating a clone of the data map gets around
+     * this issue.
+     *  
+     * @param dataMap The map data to log
+     * @return A cloned copy of the map data
+     */
+    private Map<String, Object> cloneMapData(Map<String, Object> dataMap) {
+        Map<String, Object> clonedMapData = new HashMap<>();
+        clonedMapData.putAll(dataMap);
+
+        return clonedMapData;
     }
 }
