275eae84c29e5f5726385a68a4027fa251e6784b
Ivana Pavic <ipavic@companieshouse.gov.uk>
2024-02-08 16:51:43 +0000

    Updating module version

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.235"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.243"
 
   # Environmental configuration
   environment             = var.environment
@@ -88,7 +88,7 @@ module "ecs-service" {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.229"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.243"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
