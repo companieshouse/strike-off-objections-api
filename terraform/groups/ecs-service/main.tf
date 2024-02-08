8db3b451bc625a3f456ef1e3dfddc79623d795c0
Ivana Pavic <ipavic@companieshouse.gov.uk>
2024-02-08 16:42:20 +0000

    Adding private lb support

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -33,6 +33,12 @@ module "ecs-service" {
   lb_listener_arn                 = data.aws_lb_listener.service_lb_listener.arn
   lb_listener_rule_priority       = local.lb_listener_rule_priority
   lb_listener_paths               = local.lb_listener_paths
+  ∏multilb_listeners               = {
+      "priv-api-lb": {
+          listener_arn           = data.aws_lb_listener.secondary_lb_listener.arn,
+          load_balancer_arn      = data.aws_lb.secondary_lb.arn
+      }
+  } 
 
   # ECS Task container health check
   use_task_container_healthcheck = true
