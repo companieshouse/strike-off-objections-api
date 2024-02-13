223305857a6fec2fac750c69e55c88046d943c8f
bcullerton <bcullerton@companieshouse.gov.uk>
2024-02-13 10:03:09 +0000

    Setup strike-off on internal lb only

--- terraform/groups/ecs-service/data.tf
+++ terraform/groups/ecs-service/data.tf
@@ -34,29 +34,11 @@ data "aws_iam_role" "ecs_cluster_iam_role" {
 }
 
 data "aws_lb" "service_lb" {
-  name = "${var.environment}-chs-apichgovuk"
+  name = "${var.environment}-chs-internalapi"
 }
 
 data "aws_lb_listener" "service_lb_listener" {
   load_balancer_arn = data.aws_lb.service_lb.arn
-  port = 443
-}
-
-data "aws_lb" "secondary_lb" {
-  name = "${var.environment}-chs-apichgovuk-private"
-}
-
-data "aws_lb_listener" "secondary_lb_listener" {
-  load_balancer_arn = data.aws_lb.secondary_lb.arn
-  port = 443
-}
-
-data "aws_lb" "third_lb" {
-  name = "${var.environment}-chs-internalapi"
-}
-
-data "aws_lb_listener" "third_lb_listener" {
-  load_balancer_arn = data.aws_lb.third_lb.arn
   port = 443
 }
 
