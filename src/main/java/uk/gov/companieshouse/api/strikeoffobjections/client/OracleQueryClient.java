66e9f982ae5a935ee2f4db60b3d0922fc7bf5bb4
burhanb99 <burhanboxwala@gmail.com>
2025-04-16 16:42:57 +0100

    Migrate calls to private-api-sdk

--- src/main/java/uk/gov/companieshouse/api/strikeoffobjections/client/OracleQueryClient.java
+++ src/main/java/uk/gov/companieshouse/api/strikeoffobjections/client/OracleQueryClient.java
@@ -2,13 +2,16 @@ package uk.gov.companieshouse.api.strikeoffobjections.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.model.company.ActionCodeJson;
+import uk.gov.companieshouse.api.model.company.Gaz2TransactionJson;
 import uk.gov.companieshouse.api.strikeoffobjections.common.ApiLogger;
-import uk.gov.companieshouse.api.strikeoffobjections.exception.UnsafeUrlException;
-
-import static uk.gov.companieshouse.api.strikeoffobjections.exception.UnsafeUrlException.ExceptionType.UNSAFE_URL;
+import uk.gov.companieshouse.api.strikeoffobjections.exception.OracleQueryClientException;
+import uk.gov.companieshouse.api.strikeoffobjections.service.impl.ApiSdkClient;
+import java.util.HashMap;
 
 @Component
 public class OracleQueryClient {
@@ -19,35 +22,76 @@ public class OracleQueryClient {
     @Autowired
     private ApiLogger apiLogger;
 
+    @Autowired
+    ApiSdkClient apiSdkClient;
+
     @Value("${ORACLE_QUERY_API_URL}")
     private String oracleQueryApiUrl;
 
+    private static final String COMPANY_NUMBER = "company_number";
+    private static final String ACTION_CODE_URI_SUFFIX = "/company/%s/action-code";
+    private static final String GAZ2_REQUESTED_URI_SUFFIX = "/company/%s/gaz2-requested";
+
+
     public Long getCompanyActionCode(String companyNumber, String requestId) {
-        String getCompanyActionCodeUrl = formatUrl(companyNumber, "action-code");
-        apiLogger.infoContext(requestId, "Calling Oracle Query APi at: " + getCompanyActionCodeUrl);
 
-        // Need this check for cwe, owasp-a5, sans-top25-risky
-        if (!getCompanyActionCodeUrl.startsWith(oracleQueryApiUrl)) {
-            throw new UnsafeUrlException(UNSAFE_URL, getCompanyActionCodeUrl);
+        var logMap = new HashMap<String, Object>();
+        try {
+            logMap.put(COMPANY_NUMBER, companyNumber);
+            apiLogger.infoContext(requestId, "Retrieving Action Code for Company Number ", logMap);
+            apiLogger.debugContext(requestId, "Oracle query API URL: " +  oracleQueryApiUrl);
+
+            var internalApiClient = apiSdkClient.getInternalApiClient();
+            internalApiClient.setBasePath(oracleQueryApiUrl);
+            ActionCodeJson apiResponse = internalApiClient
+                    .privateCompanyResourceHandler()
+                    .getActionCode(String.format(ACTION_CODE_URI_SUFFIX, companyNumber))
+                    .execute()
+                    .getData();
+
+            return Long.parseLong(apiResponse.getActionCode());
+
+        } catch (ApiErrorResponseException e) {
+            apiLogger.errorContext(requestId, "Error Retrieving Registered Action Code for Company ", e, logMap);
+            throw new OracleQueryClientException("Error Retrieving Registered Action Code for Company");
+        } catch (URIValidationException e) {
+            apiLogger.errorContext(requestId, "Company number invalid", e, logMap);
+            throw new OracleQueryClientException("Company number invalid");
+        } catch (NumberFormatException e) {
+            apiLogger.errorContext(requestId, "Action Code could not be parsed from the response json", e, logMap);
+            throw new OracleQueryClientException("Action Code could not be parsed from the response json");
         }
-
-        ResponseEntity<Long> response = restTemplate.getForEntity(getCompanyActionCodeUrl, Long.class);
-
-        return response.getBody();
     }
 
     public String getRequestedGaz2(String companyNumber, String requestId) {
-        String getRequestedGaz2Url = formatUrl(companyNumber, "gaz2-requested");
-        apiLogger.infoContext(requestId, "Calling Oracle Query APi at: " + getRequestedGaz2Url);
 
-        // Need this check for cwe, owasp-a5, sans-top25-risky
-        if (!getRequestedGaz2Url.startsWith(oracleQueryApiUrl)) {
-            throw new UnsafeUrlException(UNSAFE_URL, getRequestedGaz2Url);
+        var logMap = new HashMap<String, Object>();
+        try {
+            logMap.put(COMPANY_NUMBER, companyNumber);
+            apiLogger.infoContext(requestId, "Retrieving Gaz2 Data for Company Number ", logMap);
+            apiLogger.debugContext(requestId, "Oracle query API URL: " +  oracleQueryApiUrl);
+
+            var internalApiClient = apiSdkClient.getInternalApiClient();
+            internalApiClient.setBasePath(oracleQueryApiUrl);
+            Gaz2TransactionJson apiResponse = internalApiClient
+                    .privateCompanyResourceHandler()
+                    .getGaz2Requested(String.format(GAZ2_REQUESTED_URI_SUFFIX, companyNumber))
+                    .execute()
+                    .getData();
+
+            if (apiResponse == null) {
+                return null;
+            }
+
+            return apiResponse.getId();
+
+        } catch (ApiErrorResponseException e) {
+            apiLogger.errorContext(requestId, "Error Retrieving Gaz2 data for Company ", e, logMap);
+            throw new OracleQueryClientException("Error Retrieving Gaz2 data for Company");
+        } catch (URIValidationException e) {
+            apiLogger.errorContext(requestId, "Company number invalid", e, logMap);
+            throw new OracleQueryClientException("Company number invalid");
         }
-
-        ResponseEntity<String> response = restTemplate.getForEntity(getRequestedGaz2Url, String.class);
-
-        return response.getBody();
     }
 
     protected String formatUrl(String companyNumber, String action) {
--- src/main/java/uk/gov/companieshouse/api/strikeoffobjections/service/impl/ApiSdkClient.java
+++ src/main/java/uk/gov/companieshouse/api/strikeoffobjections/service/impl/ApiSdkClient.java
@@ -2,6 +2,7 @@ package uk.gov.companieshouse.api.strikeoffobjections.service.impl;
 
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.ApiClient;
+import uk.gov.companieshouse.api.InternalApiClient;
 import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
 
 /**
@@ -14,5 +15,9 @@ public class ApiSdkClient {
 
     public ApiClient getApiClient() {
         return ApiSdkManager.getSDK();
+    }
+
+    public InternalApiClient getInternalApiClient() {
+        return ApiSdkManager.getPrivateSDK();
     }
 }
